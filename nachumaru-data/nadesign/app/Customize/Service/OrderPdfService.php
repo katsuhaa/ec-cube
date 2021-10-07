75cf1fce8540bc3a4a3879ff7c25bc3c4fc1605c
katsu <katsu.negishi@gmail.com>
2021-10-07 22:24:20 +0900

    納品書に２０歳未満への販売禁止の注意喚起を記載

---
+++ nachumaru-data/nadesign/app/Customize/Service/OrderPdfService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Customize\Service;
+
+class OrderPdfService extends \Eccube\Service\OrderPdfService
+{
+
+    /**
+     * PDFに備考を設定数.
+     *
+     * @param array $formData
+     */
+    protected function renderEtcData(array $formData)
+    {
+        // フォント情報のバックアップ
+        $this->backupFont();
+
+        $this->Cell(0, 10, '', 0, 1, 'C', 0, '');
+
+        $this->SetFont(self::FONT_GOTHIC, '', 12);
+        $this->MultiCell(0, 6, '未成年者の飲酒は法律で禁じられています。', 'T', 2, 'L', 0, '');
+        $this->Ln();
+        $this->MultiCell(0, 6, '未成年者の方への酒類販売は致しません。', '', 2, 'L', 0, '');
+        
+        $this->SetFont(self::FONT_SJIS, '', 8);
+
+        // フォント情報の復元
+        $this->restoreFont();
+
+        return parent::renderEtcData($formData);
+    }
+}

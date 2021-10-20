3e7a824efcbe31ce3169eb4238430facc367fbdf
katsu <katsu.negishi@gmail.com>
2021-10-20 22:14:11 +0900

    未成年の言い回しは使わない　２０歳未満が正解

--- nachumaru-data/nadesign/app/Customize/Service/OrderPdfService.php
+++ nachumaru-data/nadesign/app/Customize/Service/OrderPdfService.php
@@ -18,9 +18,9 @@ class OrderPdfService extends \Eccube\Service\OrderPdfService
         $this->Cell(0, 10, '', 0, 1, 'C', 0, '');
 
         $this->SetFont(self::FONT_GOTHIC, '', 12);
-        $this->MultiCell(0, 6, '未成年者の飲酒は法律で禁じられています。', 'T', 2, 'L', 0, '');
+        $this->MultiCell(0, 6, '２０歳未満の方の飲酒は法律で禁じられています。', 'T', 2, 'L', 0, '');
         $this->Ln();
-        $this->MultiCell(0, 6, '未成年者の方への酒類販売は致しません。', '', 2, 'L', 0, '');
+        $this->MultiCell(0, 6, '２０歳未満の方への酒類販売は致しません。', '', 2, 'L', 0, '');
         
         $this->SetFont(self::FONT_SJIS, '', 8);
 

6b5ce396c96bb570b156d57a43f70c9ebfc8fd7d
Katsuharu Negishi <katsu.negishi@gmail.com>
2022-01-07 08:58:42 +0900

    certbotの証明書取得を更新　バグ修正と証明書の強制更新を追加

--- nachumaru-certbot-renew.sh
+++ nachumaru-certbot-renew.sh
@@ -3,6 +3,6 @@
 # コマンドはシェルがないので絶対パス ttyがないため docker-compose exec に-Tオプション追加
 cd `dirname $0`
 /usr/local/bin/docker-compose -f docker-compose.nachu.yml exec -T ec-cube certbot renew
-/usr/local/bin/docker-compose -f docker-compose.nachu.yml exec -T apachectl restart
+/usr/local/bin/docker-compose -f docker-compose.nachu.yml exec -T ec-cube apachectl restart
 cd -
 

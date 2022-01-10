2dae8a6b05abaffec0330bc1fdd367d42b93c7eb
Katsuharu Negishi <katsu.negishi@gmail.com>
2022-01-11 00:50:35 +0900

    certbot renewがdry-runでした

--- nachumaru-certbot-renew.sh
+++ nachumaru-certbot-renew.sh
@@ -2,7 +2,7 @@
 # dockerの中で証明書の更新をするスクリプト
 # コマンドはシェルがないので絶対パス ttyがないため docker-compose exec に-Tオプション追加
 cd `dirname $0`
-/usr/local/bin/docker-compose -f docker-compose.nachu.yml exec -T ec-cube certbot --dry-run renew
+/usr/local/bin/docker-compose -f docker-compose.nachu.yml exec -T ec-cube certbot renew
 if [ $? = 0 ]; then
     docker cp `docker-compose ps -q ec-cube`:/etc/letsencrypt ./nachumaru-data/
 fi

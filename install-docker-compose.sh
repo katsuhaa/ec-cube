223a9af8cf2667d7fdf1e7b4198019465ef492df
Katsuharu <katsu.negishi@gmail.com>
2022-01-12 13:41:09 +0000

    docker-composeのインストールスクリプトをarm64対応のバージョンに変更

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/2.2.3/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
